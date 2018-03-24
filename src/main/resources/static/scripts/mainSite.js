3d5956d3fa0c738da659af3a4ad7057c1eec103b
kane16 <lukasz.guminski629@gmail.com>
2018-03-24 15:31:32 +0100

    Admin site done.

--- src/main/resources/static/scripts/mainSite.js
+++ src/main/resources/static/scripts/mainSite.js
@@ -19,24 +19,15 @@
     var $myCarousel = $('#carousel-example-generic'),
         $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
 
-    //Initialize carousel
-    $myCarousel.carousel();
 
     //Animate captions in first slide on page load
     doAnimations($firstAnimatingElems);
-
-    //Pause carousel
-    $myCarousel.carousel('pause');
 
 
     //Other slides to be animated on carousel slide event
     $myCarousel.on('slide.bs.carousel', function (e) {
         var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
         doAnimations($animatingElems);
-    });
-    $('#carousel-example-generic').carousel({
-        interval:3000,
-        pause: "false"
     });
 
 })(jQuery);
